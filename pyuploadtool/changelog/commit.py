8c5ed4573ff827ccc30ded68a81da2cc96506b7c
Srevin Saju <srevinsaju@sugarlabs.org>
2021-03-20 12:46:34 +0300

    feat: use NamedTuple instead of complicating the implementation

--- pyuploadtool/changelog/commit.py
+++ pyuploadtool/changelog/commit.py
@@ -1,29 +1,14 @@
+from typing import NamedTuple
+
 from github.Commit import Commit
 
 from .author import Author
 
 
-class ChangelogCommit:
-    def __init__(
-        self,
-        author: Author,
-        message: str,
-        sha: str,
-    ):
-        self._sha = sha
-        self._author = author
-        self.message = message
-
-    def __repr__(self):
-        return f"{self.__name__}"
-
-    @property
-    def author(self):
-        return self._author
-
-    @property
-    def sha(self):
-        return self._sha
+class ChangelogCommit(NamedTuple):
+    author: Author
+    message: str
+    sha: str
 
     @classmethod
     def from_github_commit(cls, commit: Commit):
